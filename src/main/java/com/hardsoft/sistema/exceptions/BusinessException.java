0f4501f987346deec0302fe126a2b406460e832c
MatheusVilela07 <matheusbuzollob5@outlook.com>
2026-06-07 17:41:51 -0300

    Delete src directory

--- src/main/java/com/hardsoft/sistema/exceptions/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.hardsoft.sistema.exceptions;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
